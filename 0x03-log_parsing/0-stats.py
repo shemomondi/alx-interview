5e272d7f5c9990a7d1b03dc8c1bed49826e9cca3
shem <125742690+shemomondi@users.noreply.github.com>
2024-02-26 01:37:30 +0300

    rotating a 2d matrix by 90 degrees clockwise in-place

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,6 @@
 !/usr/bin/python3
 """
+
 Log parsing
 """
 import sys
@@ -41,4 +42,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_metrics(file_size_total, codes_count)
         raise
-   print_metrics(file_size_total, codes_count)
+    print_metrics(file_size_total, codes_count)
